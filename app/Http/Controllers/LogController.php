c7e79f4838c5da542b3db7d930da636e025fc95f
John Durrant <john.durrant@eagleeye.com>
2019-03-11 13:48:15 +0000

    Log Controller

---
+++ app/Http/Controllers/LogController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Log;
+use Illuminate\Http\Request;
+
+
+class LogController extends Controller
+{
+
+    public function getAllLogs()
+    {
+        return response()->json(Log::all());
+    }
+
+    public function getOneLog($id)
+    {
+        return response()->json(Log::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'user_id' => 'required',
+            'type' => 'required|alpha_dash|max:12',
+            'groove_id' => 'integer',
+            'task_id' => 'integer',
+            'answer_id' => 'integer',
+            'journal_question_id' => 'integer',
+            'performed_at' => '',
+            'success_type' => 'alpha_dash|max:12',
+            'comment' => 'max:255',
+            'mood_score' => 'integer',
+            'entry' => 'max:5000'
+        ]);
+
+        $questions = Log::create($request->all());
+
+        return response()->json($questions, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $this->validate($request, [
+            'user_id' => 'required',
+            'type' => 'required|alpha_dash|max:12',
+            'groove_id' => 'integer',
+            'task_id' => 'integer',
+            'answer_id' => 'integer',
+            'journal_question_id' => 'integer',
+            'performed_at' => '',
+            'success_type' => 'alpha_dash|max:12',
+            'comment' => 'max:255',
+            'mood_score' => 'integer',
+            'entry' => 'max:5000'
+        ]);
+
+        $questions = Log::findOrFail($id);
+        $questions->update($request->all());
+
+        return response()->json($questions, 200);
+    }
+
+    public function delete($id)
+    {
+        Log::findOrFail($id)->delete();
+        return response('Successfully Deleted Log', 200);
+    }
+
+
+}
